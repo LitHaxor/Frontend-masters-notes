08efc9a44a8a6af2468a14a398d5e623a1d7d29a
Hasan Ahamed <officials.hasan@gmail.com>
2021-01-04 00:54:58 +0600

    Completed Calculator app

--- Intro-to-web-dev-v2/projects/1.Calculator-app/js/app.js
+++ Intro-to-web-dev-v2/projects/1.Calculator-app/js/app.js
@@ -1,6 +1,6 @@
 let runningNumber= 0;
 let buffer ="0";
-let previousText;
+let previousText =null;
 const screen = document.querySelector('.screen');
 
 document.querySelector('.pannel')
@@ -26,7 +26,61 @@ const handleNumber= (value)=>{
     
 }
 
+
+
+const handleSymbol = (value) =>{
+    switch(value){  
+        case 'C':
+            buffer = "0";
+            runningNumber = 0;
+            break;
+        case '=':
+            if(previousText === null)
+                return;
+            flushOperation(parseInt(buffer));
+            previousText = null;
+            buffer = "" + runningNumber;
+            runningNumber =0;
+            break;
+        case "←":
+            if(buffer.length === 1)
+                buffer= "0";
+            else 
+                buffer = buffer.substring(0, buffer.length-1);
+            break;
+        default:
+            handleMath(value);
+            break;
+
+    }
+}
+
 const reFresh = () =>{
     screen.innerText = buffer;
 }
 
+const handleMath= (value) =>{
+    const intbuffer = parseInt(buffer);
+    if(runningNumber === 0)
+    {
+        runningNumber = intbuffer;
+    }
+    else 
+        flushOperation(intbuffer);
+    
+    previousText = value;
+    buffer = "0";
+}
+
+
+const flushOperation =  (intbuffer) => {
+    console.log("here");
+    if(previousText === "+")
+        runningNumber += intbuffer;
+    else if(previousText === "-")
+        runningNumber -= intbuffer;
+    else if(previousText === "×")
+        runningNumber *= intbuffer;
+    else 
+        runningNumber /= intbuffer;
+}
