357ebf73d5df7da8f194196ec3c9891214f42be3
Hasan Ahamed <officials.hasan@gmail.com>
2021-03-06 00:34:30 +0600

    Single Data fetch- Server SR

--- next-js/src/pages/index.jsx
+++ next-js/src/pages/index.jsx
@@ -4,26 +4,21 @@ import { jsx } from 'theme-ui'
 import React from 'react'
 import Sidebar from '../components/SideBar'
 
-const Page = () => {
+const Page = ({content}) => {
     return (
         <div sx={{variant: 'containers-fluid.page'}}>
               
                 <div sx={{ height: `calc(100vh - 60px)`}}>
                     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
-                    <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+                    <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
                 </div>
             </div>      
         </div>
     )
 }
 
-export async function getStaticProps(context) {
-    console.log(context);
-    return {
-      props: {}
-    }
-}
 export default Page;
+
 
 
 
--- next-js/src/pages/notes/[id].jsx
+++ next-js/src/pages/notes/[id].jsx
@@ -1,15 +1,32 @@
 import React from 'react'
-import {useRouter} from 'next/router'
-const page = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    console.log(id);
-    return (
-        <div>
-            note page : {id}
-        </div>
-    )
+
+export default function page ({note}){
+    
+        return (
+            <div>
+               <h1>{note.title}</h1>
+            </div>
+        )
+    
 }
 
-export default page
+export async function getServerSideProps({params, req, res}) {
+    const response = await fetch(`http://localhost:3000/api/notes/${params.id}`)
+  
+    // so much power!
+    if (!response.ok) {
+      res.writeHead(302, { Location: '/notes' })
+      res.end()
+      return {props: {}}
+    }
+  
+    const {data} = await response.json()
+    
+    if (data) {
+      return {
+        props: {note: data}
+      }
+    }
+  }
+
 
