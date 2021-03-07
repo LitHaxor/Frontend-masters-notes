8e88a7423663d27d1f7fda322871f12071629f69
Hasan Ahamed <officials.hasan@gmail.com>
2021-03-07 19:48:04 +0600

    Dynamic content, static page - CMS, FS - get posts

---
+++ production-grade-nextjs/pages/blog/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Pane, majorScale } from 'evergreen-ui'
+import matter from 'gray-matter'
+import path from 'path'
+import fs from 'fs'
+import orderby from 'lodash.orderby'
+import Container from '../../components/container'
+import HomeNav from '../../components/homeNav'
+import PostPreview from '../../components/postPreview'
+import { posts, posts as postsFromCMS } from '../../content'
+
+const Blog = ({ posts }) => {
+  return (
+    <Pane>
+      <header>
+        <HomeNav />
+      </header>
+      <main>
+        <Container>
+          {posts.map((post) => (
+            <Pane key={post.title} marginY={majorScale(5)}>
+              <PostPreview post={post} />
+            </Pane>
+          ))}
+        </Container>
+      </main>
+    </Pane>
+  )
+}
+
+Blog.defaultProps = {
+  posts: [],
+}
+
+export async function getStaticProps (){
+
+  // using CMS system
+  const cmsPosts = postsFromCMS.published.map(post=>{
+     const {data } = matter(post);
+     return data;
+  })
+
+  // using filesystem
+  const postPath = path.join(process.cwd(), 'posts');
+  const filenames = fs.readdirSync(postPath);
+  const filePosts = filenames.map(name =>{
+    const fullPath = path.join(process.cwd(), 'posts', name)
+    const file= fs.readFileSync(fullPath, 'utf-8');
+    const {data}  = matter(file)
+    return data;
+  })
+
+  const posts = [...cmsPosts, ...filePosts]
+  return{
+   props: {posts}
+  }
+}
+
+export default Blog
+
+/**
+ * Need to get the posts from the
+ * fs and our CMS
+ */
